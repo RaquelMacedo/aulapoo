9c65f496f4ffbcee5bed896e272f8ab9a9b1bdd8
Raquel Macedo de Araújo <PET0047451@aluno.firjansenaisesi.com.br>
2023-08-21 08:41:54 -0300

    Teste

--- poo/src/main/java/com/poo/poo/PooApplication.java
+++ poo/src/main/java/com/poo/poo/PooApplication.java
@@ -8,6 +8,21 @@ public class PooApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PooApplication.class, args);
+veiculo carroDoArthur = new veiculo();
+carroDoArtur.proprietario="Arthur";
+carroDoArthur.renavam="12345678";
+carroDoArthur.marca="Porshe";
+carroDoArthur.modelo="cayenne";
+carroDoArthur.ano=2023;
+carroDoArthur.cor="branco";
+carroDoArthur.quilometagem=5000;
+System.out.println(carroDoArthur.proprietario);
+
+
+
+
+
+
 	}
 
 }
